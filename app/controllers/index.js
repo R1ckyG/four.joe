0e35e2e6a01aa9b61ecc4ddcfefe24a7ef54fb47
r1ckyg <rgrant@soe.ucsc.edu>
2013-03-10 18:11:14 -0700

    More new ui stuff.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,24 @@
+var settings = Alloy.createController('settings'),
+		stats = Alloy.createController('stat'),
+		courses = Alloy.createController('course');
+
 function doClick(e) {  
     alert($.label.text);
+}
+
+function openSettings(e){
+	var view = settings.getView();
+	$.nav.open(view);
+}
+
+function openStats(e){
+	var view = stats.getView();
+	$.nav.open(view);
+}
+
+function openClass(e){
+	var view = courses.getView();
+	$.nav.open(view);
 }
 
 $.index.open();
