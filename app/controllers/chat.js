86ffbc16b0046e56f460aa4af98b513453276f41
r1ckyg <rgrant@soe.ucsc.edu>
2013-03-31 21:50:12 -0700

    new chat stuff

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -5,4 +5,9 @@ exports.setChat = function(c, ch){
   chat = c.get('chats')[ch];
   Ti.API.debug('Course ' + course.get('title') + ' chat: ' + chat['title']); 
 	$.mainContainer.title = chat.title;
+	$.chat_topic.text = chat.content;
 }
+
+$.mainContainer.addEventListener('focus', function(e){
+	Ti.API.debug('Switched to chat view');
+});
